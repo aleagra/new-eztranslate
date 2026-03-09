55af4f352ae61fb4d1bd2c388dc5d0cacbad590b
Alejandro Agra <aleagra08@hotmail.com>
2026-03-09 19:59:28 -0300

    Environment Variables

--- components/services/translate.tsx
+++ components/services/translate.tsx
@@ -1,35 +1,28 @@
-import config from "@/lib/config";
-
 export async function translateText(text: string, target: string) {
   if (!text || text.trim().length === 0) return "";
 
-  const url = config.REACT_APP_TRANSLATE;
-  const body = {
-    q: text,
-    target: target,
-  };
+  const url = process.env.REACT_APP_TRANSLATE!;
+  const body = { q: text, target };
 
   try {
     const response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "X-RapidAPI-Key": config.REACT_APP_KEY,
+        "X-RapidAPI-Key": process.env.REACT_APP_KEY!,
         "X-RapidAPI-Host": "deep-translate1.p.rapidapi.com",
       },
       body: JSON.stringify(body),
     });
 
     if (!response.ok) {
-      if (response.status === 429) {
-        return "Se alcanzó el límite de peticiones.";
-      }
+      if (response.status === 429) return "Se alcanzó el límite de peticiones.";
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const data = await response.json();
     return data?.data?.translations?.translatedText || "";
-  } catch (error: any) {
+  } catch (error) {
     console.error(error);
     return "Error translating text.";
   }
